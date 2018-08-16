7041d9be7b0a7f2370178ae1bbe86282f3772a7e
Sandy Edwards <sedwardscode@gmail.com>
2018-08-16 11:25:29 -0400

    added simple api functionality

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -6,4 +6,23 @@ class Api::V1::SongsController < ApplicationController
     render json: @songs
   end
 
+  def update
+    @song.update(song_params)
+    if @note.save
+      render json: @song, status: :accepted
+    else
+      render json: { errors: @song.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def song_params
+    params.permit(:title, :artist)
+  end
+
+  def find_song
+    @song = Song.find(params[:id])
+  end
+
 end
