9baf706d9b340089c6244d5bf61e349979040a62
Sandy <sedwardscode@gmail.com>
2018-08-19 10:02:25 -0400

    updated spotify token scopes to allow audio playback in browser

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::LoginController < ApplicationController
       client_id: 'c4b56144ef3d453581292c34d556ce35',
       response_type: "code",
       redirect_uri: 'http://localhost:3000/api/v1/logging-in',
-      scope: "user-library-read user-library-modify user-top-read user-modify-playback-state playlist-modify-public",
+      scope: "user-library-read user-library-modify playlist-modify-public user-top-read playlist-modify-public user-modify-playback-state user-follow-modify user-read-currently-playing user-read-playback-state user-follow-read app-remote-control streaming user-read-birthdate user-read-email user-read-private",
       show_dialog: true
     }
 
@@ -14,5 +14,5 @@ class Api::V1::LoginController < ApplicationController
 
     redirect_to "#{url}?#{query_params.to_query}"
   end
-  
+
 end
